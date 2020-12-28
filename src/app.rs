999b6ea6a62ab47467ebe784e83bb09f26701033
Jacobus Geluk <jacobus.geluk@agnos.ai>
2020-12-28 10:19:18 +0000

    Last commit with thedodd/trunk, switching to the approach in https://github.com/yewstack/yew-wasm-pack-template

--- src/app.rs
+++ src/app.rs
@@ -2,9 +2,21 @@ use super::header::ListHeader;
 use super::item::ListItem;
 use super::list::List;
 use super::{Hovered, WeakComponentLink};
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
-// use yew_material::MatButton;
-// use yew::html;
+use web_sys::console;
+use ybc::TileCtx::{Ancestor, Child, Parent};
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    console::log_1(&"From Rust: Hello using web-sys".into());
+    alert(&format!("Hello, {}!", name));
+}
 
 pub enum Msg {
     Hover(Hovered),
@@ -22,6 +34,7 @@ impl Component for App {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        console::log_1(&"From Rust: Component for App create()".into());
         Self {
             link,
             hovered: Hovered::None,
@@ -53,27 +66,72 @@ impl Component for App {
         let letters = ('A'..='C')
             .map(|letter| html_nested! { <ListItem name=letter.to_string() on_hover=on_hover /> });
 
+        console::log_1(&"From Rust: Component for App view()".into());
+
         html! {
-            <div class="main" onmouseenter=onmouseenter>
-                <div id="firebaseui-auth-container"></div>
-                <div id="loader">{ "Loading..." }</div>
-                <h1>{ "Nested List Demo" }</h1>
-                // <MatButton label="Click me!" />
-                <List on_hover=on_hover weak_link=list_link>
-                    <ListHeader text="Calling all Rusties!" on_hover=on_hover list_link=list_link />
-                    <ListItem name="Rustin" on_hover=on_hover />
-                    <ListItem hide=true name="Rustaroo" on_hover=on_hover />
-                    <ListItem name="Rustifer" on_hover=on_hover>
-                        <div class="sublist">{ "Sublist!" }</div>
-                        <List on_hover=on_hover weak_link=sub_list_link>
-                            <ListHeader text="Sub Rusties!" on_hover=on_hover list_link=sub_list_link/>
-                            <ListItem hide=true name="Hidden Sub" on_hover=on_hover />
-                            { for letters }
+            <>
+            <ybc::Navbar
+                classes="is-success"
+                padded=true
+                navbrand=html!{
+                    <ybc::NavbarItem>
+                        <ybc::Title
+                            classes="has-text-white"
+                            size=ybc::HeaderSize::Is4
+                        >{"Nirvana KG/UX"}</ybc::Title>
+                    </ybc::NavbarItem>
+                }
+                navstart=html!{}
+                navend=html!{
+                    <>
+                    <ybc::NavbarItem>
+                        <ybc::ButtonAnchor
+                            classes="is-black is-outlined"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            href="https://github.com/agnos-ai/kg-ux-rust">
+                            {"Github Repo"}
+                        </ybc::ButtonAnchor>
+                    </ybc::NavbarItem>
+                    <ybc::NavbarItem>
+                        <ybc::ButtonAnchor
+                            classes="is-black is-outlined"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            href="/login">
+                            {"Login"}
+                        </ybc::ButtonAnchor>
+                    </ybc::NavbarItem>
+                    </>
+                }
+            />
+            <ybc::Hero
+                classes="is-light"
+                size=ybc::HeroSize::FullheightWithNavbar
+                body=html!{
+                    <div class="main" onmouseenter=onmouseenter>
+                        <div id="firebaseui-auth-container"></div>
+                        <div id="loader">{ "Loading..." }</div>
+                        <h1>{ "Nested List Demo" }</h1>
+                        // <MatButton label="Click me!" />
+                        <List on_hover=on_hover weak_link=list_link>
+                            <ListHeader text="Calling all Rusties!" on_hover=on_hover list_link=list_link />
+                            <ListItem name="Rustin" on_hover=on_hover />
+                            <ListItem hide=true name="Rustaroo" on_hover=on_hover />
+                            <ListItem name="Rustifer" on_hover=on_hover>
+                                <div class="sublist">{ "Sublist!" }</div>
+                                <List on_hover=on_hover weak_link=sub_list_link>
+                                    <ListHeader text="Sub Rusties!" on_hover=on_hover list_link=sub_list_link/>
+                                    <ListItem hide=true name="Hidden Sub" on_hover=on_hover />
+                                    { for letters }
+                                </List>
+                            </ListItem>
                         </List>
-                    </ListItem>
-                </List>
-                { self.view_last_hovered() }
-            </div>
+                        { self.view_last_hovered() }
+                    </div>
+                }>
+            </ybc::Hero>
+            </>
         }
     }
 }
